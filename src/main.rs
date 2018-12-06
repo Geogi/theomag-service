a12082995b868ac9e1a7b187464ad296282fe1b7
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2018-12-06 16:05:08 +0100

    initial responder
    simple pong

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 extern crate actix_web;
+extern crate env_logger;
 extern crate futures;
 
-use actix_web::{http, server, App, HttpResponse, Responder, Json, HttpMessage, AsyncResponder};
+use actix_web::{http, middleware, server, App, HttpResponse, Responder, Json, HttpMessage, AsyncResponder};
 use actix_web::HttpRequest;
-use actix_web::Error;
 use futures::Future;
+use actix_web::Error;
 
 #[derive(Debug, Deserialize)]
 struct Payload {
-    //ptos: String,
+    message: String,
 }
 
 #[derive(Serialize)]
@@ -21,23 +22,33 @@ struct Response {
 
 fn check(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
     req.json()
-        .from_err()  // convert all errors into `Error`
+        .from_err()
         .and_then(|val: Payload| {
             println!("model: {:?}", val);
-            let response = vec![Response{message: String::from("uie")}];
-            Ok(HttpResponse::Ok().json(response))  // <- send response
+            let pong = vec![Response{ message: "pong".to_string() }];
+            Ok(HttpResponse::Ok().json(pong))
         })
         .responder()
 }
 
 fn main() {
+    env_logger::init();
     server::new(|| {
         App::new()
-            .resource("/check", |r|
-                r.method(http::Method::POST).f(check),
-            )
+            .middleware(middleware::Logger::default())
+            .configure(|app| {
+            middleware::cors::Cors::for_app(app)
+                .allowed_origin("http://localhost:3000")
+                .allowed_methods(vec!["GET", "POST"])
+                .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                .allowed_header(http::header::CONTENT_TYPE)
+                .max_age(3600)
+                .resource("/check", |r| {
+                    r.method(http::Method::POST).f(check);
+                })
+                .register()})
     })
-        .bind("127.0.0.1:8102")
+        .bind("127.0.0.1:8000")
         .expect("Can not bind to port 8000")
         .run();
 }
