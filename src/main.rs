54e93e3e9c06beb54cddb04e267e06e161e7b176
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2018-12-06 17:35:48 +0100

    dummy test works

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
 extern crate serde;
 extern crate serde_json;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 extern crate actix_web;
 extern crate env_logger;
 extern crate futures;
 
-use actix_web::{http, middleware, server, App, HttpResponse, Responder, Json, HttpMessage, AsyncResponder};
+use actix_web::{http, middleware, server, App, HttpResponse, HttpMessage, AsyncResponder};
 use actix_web::HttpRequest;
 use futures::Future;
 use actix_web::Error;
 
-#[derive(Debug, Deserialize)]
+#[derive(Deserialize)]
 struct Payload {
-    message: String,
+    equipments: Vec<PayloadEquipment>,
+    routes: Vec<PayloadRoute>,
+}
+
+#[derive(Deserialize)]
+struct PayloadEquipment {
+    key: String,
+    capacity: Option<u64>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct PayloadRoute {
+    upstream_key: String,
+    downstream_key: String,
 }
 
 #[derive(Serialize)]
 struct Response {
-    message: String,
+    errors: Vec<String>,
 }
 
-fn check(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
+fn check(req: &HttpRequest) -> Box<Future<Item=HttpResponse, Error=Error>> {
     req.json()
         .from_err()
-        .and_then(|val: Payload| {
-            println!("model: {:?}", val);
-            let pong = vec![Response{ message: "pong".to_string() }];
-            Ok(HttpResponse::Ok().json(pong))
+        .and_then(|payload: Payload| {
+            let dummy_response = Response {
+                errors: vec![format!("test: {} equipments, {} routes", payload.equipments.len(), payload.routes.len())]
+            };
+            Ok(HttpResponse::Ok().json(dummy_response))
         })
         .responder()
 }
@@ -37,16 +53,17 @@ fn main() {
         App::new()
             .middleware(middleware::Logger::default())
             .configure(|app| {
-            middleware::cors::Cors::for_app(app)
-                .allowed_origin("http://localhost:3000")
-                .allowed_methods(vec!["GET", "POST"])
-                .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-                .allowed_header(http::header::CONTENT_TYPE)
-                .max_age(3600)
-                .resource("/check", |r| {
-                    r.method(http::Method::POST).f(check);
-                })
-                .register()})
+                middleware::cors::Cors::for_app(app)
+                    .allowed_origin("http://localhost:3000")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                    .allowed_header(http::header::CONTENT_TYPE)
+                    .max_age(3600)
+                    .resource("/check", |r| {
+                        r.method(http::Method::POST).f(check);
+                    })
+                    .register()
+            })
     })
         .bind("127.0.0.1:8000")
         .expect("Can not bind to port 8000")
