212b1ac455deedce6b5d46fde1eeaa6c5cf2b6d3
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2018-12-06 13:14:46 +0100

    WIP (will try the example instead)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,43 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest, Responder};
+extern crate futures;
 
-fn greet(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+use actix_web::{http, server, App, HttpResponse, Responder, Json, HttpMessage, AsyncResponder};
+use actix_web::HttpRequest;
+use actix_web::Error;
+use futures::Future;
+
+#[derive(Debug, Deserialize)]
+struct Payload {
+    //ptos: String,
+}
+
+#[derive(Serialize)]
+struct Response {
+    message: String,
+}
+
+fn check(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
+    req.json()
+        .from_err()  // convert all errors into `Error`
+        .and_then(|val: Payload| {
+            println!("model: {:?}", val);
+            let response = vec![Response{message: String::from("uie")}];
+            Ok(HttpResponse::Ok().json(response))  // <- send response
+        })
+        .responder()
 }
 
 fn main() {
     server::new(|| {
         App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
+            .resource("/check", |r|
+                r.method(http::Method::POST).f(check),
+            )
     })
-        .bind("127.0.0.1:8000")
+        .bind("127.0.0.1:8102")
         .expect("Can not bind to port 8000")
         .run();
 }
